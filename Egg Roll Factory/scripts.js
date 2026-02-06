4f770f6286d14d4474f83323879e8e04b5963673
Aaron Cornelius <monsterrmann@hotmail.com>
2026-02-06 10:48:53 -0800

    Refactor variable initialization and DOM updates

--- Egg Roll Factory/scripts.js
+++ Egg Roll Factory/scripts.js
@@ -1,44 +1,43 @@
-clicks = Number(localStorage.getItem('clicks'));
-if (Number(localStorage.getItem('cpc')) > 0) {
-    cpc = Number(localStorage.getItem('cpc'));
+let clicks = Number(getItem('clicks'));
+let cpc = Number(getItem('cpc')) > 0 ? Number(getItem('cpc')) : 1;
+let cps = Number(getItem('cps'));
+let xp_prog = Number(getItem('exp_prog'));
+let exp_mult = Number(getItem('exp_mult')) > 0 ? Number(getItem('exp_mult')) : 0.5;
+let level = Number(getItem('level')) > 0 ? Number(getItem('level')) : 1
+let boomprog = 0;
+
+let levelElem = document.getElementById('level');
+let counterElem = document.getElementById('counter');
+let cpcElem = document.getElementById('cpc');
+let cpsElem = document.getElementById('cps');
+let toNextLevelElem = document.getElementById('to_next_level');
+
+function getItem(name) {
+    return localstorage.getItem(name);
 }
-else {
-    cpc = 1;
-}
-cps = Number(localStorage.getItem('cps'));
-exp_prog = Number(localStorage.getItem('exp_prog'));
-if (Number(localStorage.getItem('exp_mult')) > 0) {
-    exp_mult = Number(localStorage.getItem('exp_mult'));
-}
-else {
-    exp_mult = .5;
-}
-if (Number(localStorage.getItem('level')) > 0) {
-    level = Number(localStorage.getItem('level'));
-}
-else {
-    level = 1;
-}
-boomprog = 0;
+
 function loadpage() {
-    document.getElementById('level').innerHTML=level;
-    document.getElementById('counter').innerHTML=Math.floor(clicks);
-    document.getElementById('cpc').innerHTML=cpc;
-    document.getElementById('cps').innerHTML=cps;
-    if (Number(localStorage.getItem('to_next_level')) > 0) {
-        document.getElementById('to_next_level').innerHTML = localStorage.getItem('to_next_level');
+    levelElem.innerHTML=level;
+    counterElem.innerHTML=Math.floor(clicks);
+    cpcElem.innerHTML=cpc;
+    cpsElem.innerHTML=cps;
+    if (Number(getItem('to_next_level')) > 0) {
+        toNextLevelElem.innerHTML = getItem('to_next_level');
     }
 }
+
 function startcps() {
     setInterval(continuecps,10);
 }
+
 function continuecps() {
     clicks = clicks + cps*.01;
     exp_prog = exp_prog + cps*.01;
-    document.getElementById('counter').innerHTML=Math.floor(clicks);
+    counterElem.innerHTML=Math.floor(clicks);
     localStorage.setItem('clicks', clicks);
     exp_up();
 }
+
 function clicked() {
     clicks = clicks + cpc;
     exp_prog = exp_prog + cpc;
@@ -46,16 +45,17 @@ function clicked() {
     if (boomprog == 100) {
         boom();
     }
-    document.getElementById('counter').innerHTML=Math.floor(clicks);
+    counterElem.innerHTML=Math.floor(clicks);
     localStorage.setItem('clicks', clicks);
     exp_up();
 }
+
 function exp_up() {
     localStorage.setItem('exp_prog', exp_prog);
     document.getElementById('exp_counter').innerHTML=Math.floor(exp_prog);
     document.getElementById('exp').style.width=exp_prog*exp_mult + "%";
-    if (Number(document.getElementById('exp_counter').innerHTML) > Number(document.getElementById('to_next_level').innerHTML)) {
-        exp_prog = exp_prog - Number(document.getElementById('to_next_level').innerHTML);
+    if (Number(document.getElementById('exp_counter').innerHTML) > Number(toNextLevelElem.innerHTML)) {
+        exp_prog = exp_prog - Number(toNextLevelElem.innerHTML);
         localStorage.setItem('exp_prog', exp_prog);
         document.getElementById('exp_counter').innerHTML=Math.floor(exp_prog);
         lvl_up();
@@ -63,23 +63,26 @@ function exp_up() {
         exp_up();
     }
 }
+
 function lvl_up() {
     level = level + 1;
     localStorage.setItem('level', level);
-    document.getElementById('level').innerHTML=level;
+    levelElem.innerHTML=level;
     if (level == 2) {
-        document.getElementById('to_next_level').innerHTML=1000;
+        toNextLevelElem.innerHTML=1000;
         exp_mult = exp_mult/10
         localStorage.setItem('exp_mult', exp_mult);
-        localStorage.setItem('to_next_level', Number(document.getElementById('to_next_level').innerHTML));
+        localStorage.setItem('to_next_level', Number(toNextLevelElem.innerHTML));
     }
+        
     else {
-        document.getElementById('to_next_level').innerHTML=document.getElementById('to_next_level').innerHTML + "000";
+        toNextLevelElem.innerHTML=toNextLevelElem.innerHTML + "000";
         exp_mult = exp_mult/1000
         localStorage.setItem('exp_mult', exp_mult);
-        localStorage.setItem('to_next_level', Number(document.getElementById('to_next_level').innerHTML));
+        localStorage.setItem('to_next_level', Number(toNextLevelElem.innerHTML));
     }
 }
+
 function boom() {
     boomprog = 0;
     document.getElementById('image').src="explosion.gif";
@@ -87,36 +90,43 @@ function boom() {
         document.getElementById('image').src="eggroll.png";
     }, 800);
 }
+
 function upgrade(resourceCost, increment, type) {
     if (document.getElementById("buy_all").checked) {
         var available = Math.floor(clicks/resourceCost);
         clicks = clicks - resourceCost*available;
         document.getElementById("counter").innerHTML = Math.floor(clicks);
+        
         if (type == 'cpc') {
             cpc += available*increment;
-            document.getElementById('cpc').innerHTML = cpc;
+            cpcElem.innerHTML = cpc;
             localStorage.setItem('cpc', cpc);
         }
+            
         else if (type == 'cps') {
             cps += available*increment;
-            document.getElementById('cps').innerHTML = cps;
+            cpsElem.innerHTML = cps;
             localStorage.setItem('cps', cps);
         }
     }
+        
     else if (clicks >= resourceCost) {
         clicks -= resourceCost;
-        document.getElementById('counter').innerHTML = Math.floor(clicks);
+        counterElem.innerHTML = Math.floor(clicks);
+        
         if (type == 'cpc') {
             cpc += increment;
-            document.getElementById('cpc').innerHTML = cpc;
+            cpcElem.innerHTML = cpc;
             localStorage.setItem('cpc', cpc);
         }
+            
         else if (type == 'cps') {
             cps += increment;
-            document.getElementById('cps').innerHTML = cps;
+            cpsElem.innerHTML = cps;
             localStorage.setItem('cps', cps);
         }
     }
+        
     else {
         alert("Not Enough Egg Rolls!");
     }
@@ -137,12 +147,12 @@ function reset() {
             localStorage.setItem('level', level);
             localStorage.setItem('exp_mult', exp_mult);
             localStorage.setItem('exp_prog', exp_prog);
-            document.getElementById('level').innerHTML=level;
-            document.getElementById('to_next_level').innerHTML="100";
-            localStorage.setItem('to_next_level', Number(document.getElementById('to_next_level').innerHTML));
-            document.getElementById('counter').innerHTML=clicks;
-            document.getElementById('cpc').innerHTML=cpc;
-            document.getElementById('cps').innerHTML=cps;
+            levelElem.innerHTML=level;
+            toNextLevelElem.innerHTML="100";
+            localStorage.setItem('to_next_level', Number(toNextLevelElem.innerHTML));
+            counterElem.innerHTML=clicks;
+            cpcElem.innerHTML=cpc;
+            cpsElem.innerHTML=cps;
         }
     }
 }
