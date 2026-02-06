fa9410e6c666a9d7d208954c5b79ec1d1033d395
Aaron Cornelius <monsterrmann@hotmail.com>
2026-02-06 11:04:43 -0800

    Refactor localStorage access methods
    Refactor localStorage usage to use setItem and getItem functions for better code organization and readability.

--- Egg Roll Factory/scripts.js
+++ Egg Roll Factory/scripts.js
@@ -1,3 +1,11 @@
+function getItem(name) {
+    return localstorage.getItem(name);
+}
+
+function setItem(name, value) {
+    return localstorage.setItem(name, value);
+}
+
 let clicks = Number(getItem('clicks'));
 let cpc = Number(getItem('cpc')) > 0 ? Number(getItem('cpc')) : 1;
 let cps = Number(getItem('cps'));
@@ -11,10 +19,9 @@ let counterElem = document.getElementById('counter');
 let cpcElem = document.getElementById('cpc');
 let cpsElem = document.getElementById('cps');
 let toNextLevelElem = document.getElementById('to_next_level');
-
-function getItem(name) {
-    return localstorage.getItem(name);
-}
+let imageElem = document.getElementById('image');
+let expCounterElem = document.getElementById('exp_counter');
+let expElem = document.getElementById('exp');
 
 function loadpage() {
     levelElem.innerHTML=level;
@@ -31,63 +38,65 @@ function startcps() {
 }
 
 function continuecps() {
-    clicks = clicks + cps*.01;
-    exp_prog = exp_prog + cps*.01;
-    counterElem.innerHTML=Math.floor(clicks);
-    localStorage.setItem('clicks', clicks);
+    clicks += cps*.01;
+    exp_prog += cps*.01;
+    counterElem.innerHTML = Math.floor(clicks);
+    setItem('clicks', clicks);
     exp_up();
 }
 
 function clicked() {
-    clicks = clicks + cpc;
-    exp_prog = exp_prog + cpc;
-    boomprog = boomprog + 1;
-    if (boomprog == 100) {
+    clicks += cpc;
+    exp_prog += cpc;
+    boomprog++;
+    
+    if (boomprog === 100) {
         boom();
     }
-    counterElem.innerHTML=Math.floor(clicks);
-    localStorage.setItem('clicks', clicks);
+    
+    counterElem.innerHTML = Math.floor(clicks);
+    setItem('clicks', clicks);
     exp_up();
 }
 
 function exp_up() {
-    localStorage.setItem('exp_prog', exp_prog);
-    document.getElementById('exp_counter').innerHTML=Math.floor(exp_prog);
-    document.getElementById('exp').style.width=exp_prog*exp_mult + "%";
-    if (Number(document.getElementById('exp_counter').innerHTML) > Number(toNextLevelElem.innerHTML)) {
+    setItem('exp_prog', exp_prog);
+    expCounterElem.innerHTML=Math.floor(exp_prog);
+    expElem.style.width=exp_prog*exp_mult + "%";
+    if (Number(expCounterElem.innerHTML) > Number(toNextLevelElem.innerHTML)) {
         exp_prog = exp_prog - Number(toNextLevelElem.innerHTML);
-        localStorage.setItem('exp_prog', exp_prog);
-        document.getElementById('exp_counter').innerHTML=Math.floor(exp_prog);
+        setItem('exp_prog', exp_prog);
+        expCounterElem.innerHTML=Math.floor(exp_prog);
         lvl_up();
-        document.getElementById('exp').style.width = exp_prog*exp_mult + "%";
+        expElem.style.width = exp_prog*exp_mult + "%";
         exp_up();
     }
 }
 
 function lvl_up() {
-    level = level + 1;
-    localStorage.setItem('level', level);
-    levelElem.innerHTML=level;
-    if (level == 2) {
-        toNextLevelElem.innerHTML=1000;
-        exp_mult = exp_mult/10
-        localStorage.setItem('exp_mult', exp_mult);
-        localStorage.setItem('to_next_level', Number(toNextLevelElem.innerHTML));
+    level++;
+    setItem('level', level);
+    levelElem.innerHTML = level;
+    if (level === 2) {
+        toNextLevelElem.innerHTML = 1000;
+        exp_mult /= 1000;
+        setItem('exp_mult', exp_mult);
+        setItem('to_next_level', Number(toNextLevelElem.innerHTML));
     }
         
     else {
         toNextLevelElem.innerHTML=toNextLevelElem.innerHTML + "000";
-        exp_mult = exp_mult/1000
-        localStorage.setItem('exp_mult', exp_mult);
-        localStorage.setItem('to_next_level', Number(toNextLevelElem.innerHTML));
+        exp_mult /= 1000;
+        setItem('exp_mult', exp_mult);
+        setItem('to_next_level', Number(toNextLevelElem.innerHTML));
     }
 }
 
 function boom() {
     boomprog = 0;
-    document.getElementById('image').src="explosion.gif";
-    setTimeout(function(){
-        document.getElementById('image').src="eggroll.png";
+    imageElem.src="explosion.gif";
+    setTimeout(() => {
+        imageElem.src="eggroll.png";
     }, 800);
 }
 
@@ -95,18 +104,18 @@ function upgrade(resourceCost, increment, type) {
     if (document.getElementById("buy_all").checked) {
         var available = Math.floor(clicks/resourceCost);
         clicks = clicks - resourceCost*available;
-        document.getElementById("counter").innerHTML = Math.floor(clicks);
+        counterElem.innerHTML = Math.floor(clicks);
         
         if (type == 'cpc') {
             cpc += available*increment;
             cpcElem.innerHTML = cpc;
-            localStorage.setItem('cpc', cpc);
+            setItem('cpc', cpc);
         }
             
         else if (type == 'cps') {
             cps += available*increment;
             cpsElem.innerHTML = cps;
-            localStorage.setItem('cps', cps);
+            setItem('cps', cps);
         }
     }
         
@@ -117,13 +126,13 @@ function upgrade(resourceCost, increment, type) {
         if (type == 'cpc') {
             cpc += increment;
             cpcElem.innerHTML = cpc;
-            localStorage.setItem('cpc', cpc);
+            setItem('cpc', cpc);
         }
             
         else if (type == 'cps') {
             cps += increment;
             cpsElem.innerHTML = cps;
-            localStorage.setItem('cps', cps);
+            setItem('cps', cps);
         }
     }
         
@@ -131,28 +140,30 @@ function upgrade(resourceCost, increment, type) {
         alert("Not Enough Egg Rolls!");
     }
 }
+
 function reset() {
-    if (confirm('Are you sure you want to reset all of your progress?')) {
-        if (confirm('Are you really really sure? (You will not recieve anything.)')) {
-            clicks = 0;
-            cpc = 1;
-            cps = 0;
-            boomprog = 0;
-            exp_prog = 0;
-            exp_mult = .5;
-            level = 1;
-            localStorage.setItem('clicks', clicks);
-            localStorage.setItem('cpc', cpc);
-            localStorage.setItem('cps', cps);
-            localStorage.setItem('level', level);
-            localStorage.setItem('exp_mult', exp_mult);
-            localStorage.setItem('exp_prog', exp_prog);
-            levelElem.innerHTML=level;
-            toNextLevelElem.innerHTML="100";
-            localStorage.setItem('to_next_level', Number(toNextLevelElem.innerHTML));
-            counterElem.innerHTML=clicks;
-            cpcElem.innerHTML=cpc;
-            cpsElem.innerHTML=cps;
-        }
-    }
+    if (!confirm('Are you sure you want to reset all of your progress?'))
+        return;
+    if (!confirm('Are you really really sure? (You will not recieve anything.)'))
+        return;
+    
+    clicks = 0;
+    cpc = 1;
+    cps = 0;
+    boomprog = 0;
+    exp_prog = 0;
+    exp_mult = .5;
+    level = 1;
+    setItem('clicks', clicks);
+    setItem('cpc', cpc);
+    setItem('cps', cps);
+    setItem('level', level);
+    setItem('exp_mult', exp_mult);
+    setItem('exp_prog', exp_prog);
+    setItem('to_next_level', 100);
+    levelElem.innerHTML=level;
+    toNextLevelElem.innerHTML="100";
+    counterElem.innerHTML=clicks;
+    cpcElem.innerHTML=cpc;
+    cpsElem.innerHTML=cps;
 }
