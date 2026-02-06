e72286fae70b00058e8fd1f75249e063a5c8f4cc
Aaron Cornelius <monsterrmann@hotmail.com>
2026-02-06 11:17:07 -0800

    Remove try-catch block from getItem function

--- Egg Roll Factory/scripts.js
+++ Egg Roll Factory/scripts.js
@@ -1,4 +1,3 @@
-try{
 function getItem(name) {
     return localStorage.getItem(name);
 }
@@ -168,4 +167,3 @@ function reset() {
     cpcElem.innerHTML=cpc;
     cpsElem.innerHTML=cps;
 }
-} catch(e) {alert(e)}
